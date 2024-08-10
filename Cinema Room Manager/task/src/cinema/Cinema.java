4469def1e5b6419a41911e6a1e40d74dc003b784
topheraronson <christopheraronson@icloud.com>
2024-08-10 16:10:34 -0400

    Finished stage 2

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,19 +1,29 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        System.out.println("Cinema:");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 8; i++) {
-            System.out.print(i == 0 ? " " : i + " ");
+        System.out.println("Enter the number of rows:");
+        int numberOfRows = scanner.nextInt();
 
-            for (int j = 1; j <= 8; j++) {
-                System.out.print(i != 0 ? "S " : j + " ");
-            }
+        System.out.println("Enter the number of seats in each row:");
+        int seatsPerRow = scanner.nextInt();
 
-            System.out.println();
+        int totalSeats = numberOfRows * seatsPerRow;
+        int frontRows = numberOfRows / 2;
+        int backRows = numberOfRows - frontRows;
+        int income;
+
+        if (totalSeats < 60) {
+            income = totalSeats * 10;
+        } else {
+            income = frontRows * seatsPerRow * 10 + backRows * seatsPerRow * 8;
         }
 
+        System.out.printf("Total income: $%d", income);
     }
 }
