7cbcdc02dba0c0f09fb9635b4c7bb28bce219627
topheraronson <christopheraronson@icloud.com>
2024-08-10 14:52:46 -0400

    Second stage of project

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -3,6 +3,17 @@ package cinema;
 public class Cinema {
 
     public static void main(String[] args) {
-        // Write your code here
+        System.out.println("Cinema:");
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print(i == 0 ? " " : i + " ");
+
+            for (int j = 1; j <= 8; j++) {
+                System.out.print(i != 0 ? "S " : j + " ");
+            }
+
+            System.out.println();
+        }
+
     }
 }
