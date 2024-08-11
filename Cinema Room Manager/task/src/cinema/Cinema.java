baa40058ef374c76e65b593336bfa8a5e1277b4e
topheraronson <christopheraronson@icloud.com>
2024-08-11 13:12:34 -0400

    Finished stage 3

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -13,17 +13,39 @@ public class Cinema {
         System.out.println("Enter the number of seats in each row:");
         int seatsPerRow = scanner.nextInt();
 
-        int totalSeats = numberOfRows * seatsPerRow;
-        int frontRows = numberOfRows / 2;
-        int backRows = numberOfRows - frontRows;
-        int income;
+        System.out.println("Cinema:");
 
-        if (totalSeats < 60) {
-            income = totalSeats * 10;
-        } else {
-            income = frontRows * seatsPerRow * 10 + backRows * seatsPerRow * 8;
+        for (int i = 0; i <= numberOfRows; i++) {
+            System.out.print(i == 0 ? "  " : i + " ");
+
+            for (int j = 1; j <= seatsPerRow; j++) {
+                System.out.print(i != 0 ? "S " : j + " ");
+            }
+
+            System.out.println();
         }
 
-        System.out.printf("Total income: $%d", income);
+        System.out.println("Enter a row number:");
+        int userRow = scanner.nextInt();
+
+        System.out.println("Enter a seat number in that row:");
+        int userSeat = scanner.nextInt();
+
+        int totalSeats = numberOfRows * seatsPerRow;
+        int frontRows = numberOfRows / 2;
+
+        System.out.printf("Ticket price: $%d\n", totalSeats <= 60 ? 10 : userRow <= frontRows ? 10 : 8);
+
+        System.out.println("Cinema:");
+        for (int i = 0; i <= numberOfRows; i++) {
+            System.out.print(i == 0 ? "  " : i + " ");
+
+            for (int j = 1; j <= seatsPerRow; j++) {
+                String seatCharacter = i == userRow && j == userSeat ? "B " : "S ";
+                System.out.print(i != 0 ? seatCharacter : j + " ");
+            }
+
+            System.out.println();
+        }
     }
 }
